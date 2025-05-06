6f78e1f2de00470e2fff207f43da5e0c25a58f1a
Mariano-Lopez <marianoalopez99@gmail.com>
2025-05-06 20:05:55 -0300

    Componentes sin name.

--- pryLopezMGestionDeInventarios/frmUsuario.Designer.cs
+++ pryLopezMGestionDeInventarios/frmUsuario.Designer.cs
@@ -31,21 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUsuario));
             this.lblTit = new System.Windows.Forms.Label();
             this.lblNom = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblInstruccion = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
             this.mrcSesion = new System.Windows.Forms.GroupBox();
             this.chkContraseña = new System.Windows.Forms.CheckBox();
             this.btnIngresar = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
             this.txtContra = new System.Windows.Forms.TextBox();
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.lblIntentos = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblCIES = new System.Windows.Forms.Label();
+            this.imgIES = new System.Windows.Forms.PictureBox();
             this.imgLogo = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblGuiones = new System.Windows.Forms.Label();
             this.mrcSesion.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIES)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -71,41 +71,41 @@
             this.lblNom.TabIndex = 2;
             this.lblNom.Text = "Korax";
             // 
-            // label1
+            // lblInstruccion
             // 
-            this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label1.Location = new System.Drawing.Point(31, 105);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(443, 77);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Porfavor complete los campos para ingresar al sistema.\r\nRecuerde que solo cuenta " +
+            this.lblInstruccion.BackColor = System.Drawing.Color.Black;
+            this.lblInstruccion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblInstruccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruccion.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblInstruccion.Location = new System.Drawing.Point(31, 105);
+            this.lblInstruccion.Name = "lblInstruccion";
+            this.lblInstruccion.Size = new System.Drawing.Size(443, 77);
+            this.lblInstruccion.TabIndex = 3;
+            this.lblInstruccion.Text = "Porfavor complete los campos para ingresar al sistema.\r\nRecuerde que solo cuenta " +
     "con 3 intentos sino su cuenta se bloqueará y deberá comunicarse con el departame" +
     "nto de sistemas.";
             // 
-            // label2
+            // lblUsuario
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label2.Location = new System.Drawing.Point(6, 16);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 16);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Usuario:";
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblUsuario.Location = new System.Drawing.Point(6, 16);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(65, 16);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "Usuario:";
             // 
             // mrcSesion
             // 
             this.mrcSesion.BackColor = System.Drawing.Color.Black;
             this.mrcSesion.Controls.Add(this.chkContraseña);
             this.mrcSesion.Controls.Add(this.btnIngresar);
-            this.mrcSesion.Controls.Add(this.label3);
+            this.mrcSesion.Controls.Add(this.lblContraseña);
             this.mrcSesion.Controls.Add(this.txtContra);
             this.mrcSesion.Controls.Add(this.txtUsuario);
-            this.mrcSesion.Controls.Add(this.label2);
+            this.mrcSesion.Controls.Add(this.lblUsuario);
             this.mrcSesion.Location = new System.Drawing.Point(135, 190);
             this.mrcSesion.Name = "mrcSesion";
             this.mrcSesion.Size = new System.Drawing.Size(189, 175);
@@ -141,17 +141,17 @@
             this.btnIngresar.MouseEnter += new System.EventHandler(this.btnIngresar_MouseEnter);
             this.btnIngresar.MouseLeave += new System.EventHandler(this.btnIngresar_MouseLeave);
             // 
-            // label3
+            // lblContraseña
             // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label3.Location = new System.Drawing.Point(6, 70);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 16);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Contraseña:";
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.BackColor = System.Drawing.Color.Transparent;
+            this.lblContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblContraseña.Location = new System.Drawing.Point(6, 70);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(90, 16);
+            this.lblContraseña.TabIndex = 7;
+            this.lblContraseña.Text = "Contraseña:";
             // 
             // txtContra
             // 
@@ -182,27 +182,27 @@
             this.lblIntentos.Text = "Intentos restantes:";
             this.lblIntentos.Visible = false;
             // 
-            // label4
+            // lblCIES
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Black;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label4.Location = new System.Drawing.Point(12, 408);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(277, 16);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Coleigo Universitario IES Siglo 21 2025";
+            this.lblCIES.AutoSize = true;
+            this.lblCIES.BackColor = System.Drawing.Color.Black;
+            this.lblCIES.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCIES.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblCIES.Location = new System.Drawing.Point(12, 408);
+            this.lblCIES.Name = "lblCIES";
+            this.lblCIES.Size = new System.Drawing.Size(277, 16);
+            this.lblCIES.TabIndex = 10;
+            this.lblCIES.Text = "Coleigo Universitario IES Siglo 21 2025";
             // 
-            // pictureBox1
+            // imgIES
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(432, 377);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(53, 46);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 11;
-            this.pictureBox1.TabStop = false;
+            this.imgIES.Image = ((System.Drawing.Image)(resources.GetObject("imgIES.Image")));
+            this.imgIES.Location = new System.Drawing.Point(432, 377);
+            this.imgIES.Name = "imgIES";
+            this.imgIES.Size = new System.Drawing.Size(53, 46);
+            this.imgIES.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgIES.TabIndex = 11;
+            this.imgIES.TabStop = false;
             // 
             // imgLogo
             // 
@@ -215,16 +215,16 @@
             this.imgLogo.TabIndex = 12;
             this.imgLogo.TabStop = false;
             // 
-            // label5
+            // lblGuiones
             // 
-            this.label5.BackColor = System.Drawing.Color.Black;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label5.Location = new System.Drawing.Point(107, 67);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(288, 25);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "----------------------------------------------";
+            this.lblGuiones.BackColor = System.Drawing.Color.Black;
+            this.lblGuiones.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuiones.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblGuiones.Location = new System.Drawing.Point(107, 67);
+            this.lblGuiones.Name = "lblGuiones";
+            this.lblGuiones.Size = new System.Drawing.Size(288, 25);
+            this.lblGuiones.TabIndex = 13;
+            this.lblGuiones.Text = "----------------------------------------------";
             // 
             // frmUsuario
             // 
@@ -234,13 +234,13 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(497, 432);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblGuiones);
             this.Controls.Add(this.imgLogo);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.imgIES);
+            this.Controls.Add(this.lblCIES);
             this.Controls.Add(this.lblIntentos);
             this.Controls.Add(this.mrcSesion);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInstruccion);
             this.Controls.Add(this.lblNom);
             this.Controls.Add(this.lblTit);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -251,7 +251,7 @@
             this.Load += new System.EventHandler(this.frmUsuario_Load);
             this.mrcSesion.ResumeLayout(false);
             this.mrcSesion.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIES)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -262,17 +262,17 @@
 
         private System.Windows.Forms.Label lblTit;
         private System.Windows.Forms.Label lblNom;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblInstruccion;
+        private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.GroupBox mrcSesion;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblContraseña;
         private System.Windows.Forms.TextBox txtContra;
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Label lblIntentos;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCIES;
+        private System.Windows.Forms.PictureBox imgIES;
         private System.Windows.Forms.PictureBox imgLogo;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblGuiones;
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.CheckBox chkContraseña;
     }
